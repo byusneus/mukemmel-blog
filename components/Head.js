92b42f0483bd4f765400bec3748affe3aac00fbf
Yunus Emre Alpak <byusneus@gmail.com>
2019-12-13 00:24:09 +0300

    Anasayfa tasarımı devam ediyor - 1

--- components/Head.js
+++ components/Head.js
@@ -5,7 +5,8 @@ const Head = ({ title }) => {
         <NextHead>
             <title>{title}</title>
             <link rel="icon" href="/favicon.ico" />
-            <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700|Poppins:300,600&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,400i,600,700&display=swap" rel="stylesheet"/>
+            <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
         </NextHead>
     );
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, {useRef, useEffect} from "react";
 import Link from "next/link";
 import Head from "../components/Head";
 import styles from '../styles/Home.scss';
+import { MdAlarm, MdContentPaste } from "react-icons/md";
 import { TweenMax, Power3 } from "gsap";
 
 const Home = () => {
@@ -36,17 +37,30 @@ const Home = () => {
           <hr className="header-hr"/>
         </div>
       </header>
+      
       <section id="banner">
         <div className="row">
           <div className="box">
             <img src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
             <div className="food-info">
               <div className="food-title">
-                <h3>Bir Buse Yemeği</h3>
+                <h3>Bir Buse Yemeği 1</h3>
                 <span>Lorem ipsum merhaba dünya</span>
               </div>
               <div className="food-details">
-
+                  <div className="cooking-information">
+                        <div className="cooking-time">
+                          <MdAlarm color="white" fontSize="26"/>
+                          <span className="cooking-information-article">30 min.</span>
+                        </div>
+                        <div className="cooking-time">
+                          <MdContentPaste color="white" fontSize="26"/>
+                          <span className="cooking-information-article">5</span>
+                        </div>
+                      </div>
+                      <div className="cooking-button">
+                        <Link to="/"><a>Tarif</a></Link>
+                  </div>
               </div>
             </div>
           </div>
@@ -54,11 +68,23 @@ const Home = () => {
               <img src="https://images.pexels.com/photos/2662875/pexels-photo-2662875.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
               <div className="food-info">
                 <div className="food-title">
-                  <h3>Bir Buse Yemeği</h3>
+                  <h3>Bir Buse Yemeği 2</h3>
                   <span>Lorem ipsum merhaba dünya</span>
                 </div>
                 <div className="food-details">
-                  
+                  <div className="cooking-information">
+                    <div className="cooking-time">
+                      <MdAlarm color="white" fontSize="26"/>
+                      <span className="cooking-information-article">30 min.</span>
+                    </div>
+                    <div className="cooking-time">
+                      <MdContentPaste color="white" fontSize="26"/>
+                      <span className="cooking-information-article">5</span>
+                    </div>
+                  </div>
+                  <div className="cooking-button">
+                    <Link to="/"><a>Tarif</a></Link>
+                  </div>
                 </div>
               </div>
           </div>
@@ -66,11 +92,23 @@ const Home = () => {
           <img src="https://images.pexels.com/photos/2059193/pexels-photo-2059193.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
             <div className="food-info">
               <div className="food-title">
-                <h3>Bir Buse Yemeği</h3>
+                <h3>Bir Buse Yemeği 3</h3>
                 <span>Lorem ipsum merhaba dünya</span>
               </div>
               <div className="food-details">
-                
+                  <div className="cooking-information">
+                        <div className="cooking-time">
+                          <MdAlarm color="white" fontSize="26"/>
+                          <span className="cooking-information-article">30 min.</span>
+                        </div>
+                        <div className="cooking-time">
+                          <MdContentPaste color="white" fontSize="26"/>
+                          <span className="cooking-information-article">5</span>
+                        </div>
+                      </div>
+                      <div className="cooking-button">
+                        <Link to="/"><a>Tarif</a></Link>
+                  </div>
               </div>
             </div>
           </div>
@@ -78,11 +116,23 @@ const Home = () => {
           <img src="https://images.pexels.com/photos/675951/pexels-photo-675951.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
             <div className="food-info">
               <div className="food-title">
-                <h3>Bir Buse Yemeği</h3>
+                <h3>Bir Buse Yemeği 4</h3>
                 <span>Lorem ipsum merhaba dünya</span>
               </div>
               <div className="food-details">
-                
+                  <div className="cooking-information">
+                        <div className="cooking-time">
+                          <MdAlarm color="white" fontSize="26"/>
+                          <span className="cooking-information-article">30 min.</span>
+                        </div>
+                        <div className="cooking-time">
+                          <MdContentPaste color="white" fontSize="26"/>
+                          <span className="cooking-information-article">5</span>
+                        </div>
+                      </div>
+                      <div className="cooking-button">
+                        <Link to="/"><a>Tarif</a></Link>
+                  </div>
               </div>
             </div>
           </div>
@@ -90,15 +140,45 @@ const Home = () => {
           <img src="https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
             <div className="food-info">
               <div className="food-title">
-                <h3>Bir Buse Yemeği</h3>
+                <h3>Bir Buse Yemeği 5</h3>
                 <span>Lorem ipsum merhaba dünya</span>
               </div>
               <div className="food-details">
-                
+                      <div className="cooking-information">
+                            <div className="cooking-time">
+                              <MdAlarm color="white" fontSize="26"/>
+                              <span className="cooking-information-article">30 min.</span>
+                            </div>
+                            <div className="cooking-time">
+                              <MdContentPaste color="white" fontSize="26"/>
+                              <span className="cooking-information-article">5</span>
+                            </div>
+                          </div>
+                          <div className="cooking-button">
+                            <Link to="/"><a>Tarif</a></Link>
+                      </div>
               </div>
             </div>
           </div>
         </div>
+      </section>
+
+      <section id="categories-section">
+          <div className="container">
+            <div className="categories-container">
+              <h2 className="categories-title">TARİF KATEGORİLERİ</h2>
+              <div className="row-one">
+                <div className="c-box">
+                  <div className="c-img">
+                    <img src="https://images.pexels.com/photos/277253/pexels-photo-277253.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+                  </div>
+                  <span>SOSLAR</span>
+                </div>
+                <div className="c-box"></div>
+                <div className="c-box"></div>
+              </div>
+            </div>
+          </div>
       </section>
     <style jsx>{styles}</style>
    </div>
