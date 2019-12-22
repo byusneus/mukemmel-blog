4f174c40d2c1af3b290da887ba63b3525d67aa05
Yunus Emre Alpak <byusneus@gmail.com>
2019-12-22 04:08:03 +0300

    Scroll animasyonları için gerekli kurulumlar yapıldı ve test edildi.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-
+const path = require('path')
 module.exports = {
   webpack: (config, { defaultLoaders }) => {
     config.module.rules.push({
@@ -16,7 +16,7 @@ module.exports = {
     })
 
     return config
-  },
+  }
 }
 
 // const withPlugins = require("next-compose-plugins");
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,18 @@
 import React, { useRef, useEffect } from "react";
-import Link from "next/link";
+
 import Head from "../components/Head";
-import dynamic from "next/dynamic";
 import styles from '../styles/Home.scss';
 import { MdAlarm, MdContentPaste} from "react-icons/md";
 import PopularRecipes from "../components/PopularRecipes";
 import Categories from "../components/Categories";
-
-const popularRecipes = [
-  {
-    title: "Bir Buse Yemeği",
-    image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    recipe: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-    created: "20/09/2019"
-  },
-  {
-    title: "Bir Buse Yemeği",
-    image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    recipe: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-    created: "20/09/2019"
-  },
-  {
-    title: "Bir Buse Yemeği",
-    image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    recipe: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-    created: "20/09/2019"
-  }
-]
+import Header from "../components/Header";
 
 const Home = () => {
-
-  let logoItem = useRef(null);
-  
-
   return (
     <div className="container">
       <Head title="Home" />
 
-      <header id="header">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="header-inner">
-              <div className="logo">BUSEM</div>
-              <div className="navigation">
-                <nav>
-                  <ul>
-                    <li><Link href="/"><a>Anasayfa</a></Link></li>
-                    <li><Link href="/"><a>Kategoriler</a></Link></li>
-                    <li><Link href="/"><a>Yemeğin Hikayesi</a></Link></li>
-                    <li><Link href="/"><a>Hakkımda</a></Link></li>
-                  </ul>
-                </nav>
-              </div>
-            </div>
-          </div>
-          <hr className="header-hr" />
-        </div>
-      </header>
+      <Header/>
 
       <section id="banner">
         <div className="row">
