efccec6e73d88c1f9a47b3e0a6c3c4422b69c782
Yunus Emre Alpak <byusneus@gmail.com>
2020-01-15 07:39:06 +0300

    Anasayfada görüntülenen blogların veritabanından gelmesi ayarlandı.

--- pages/index.js
+++ pages/index.js
@@ -8,98 +8,84 @@ import computer from "../images/computer.jpg"
 import code from "../images/code.png"
 import code2 from "../images/code2.jpg"
 import next from "../images/next.png"
+import firestore from "../common/firestore";
+import { TimelineLite } from "gsap";
 
-const Home = () => {
+const Home = ({ blogList }) => {
 
-  let blogList = useRef(null);
+  let blogListHtml = useRef(null);
   let pagination = useRef(null);
 
+  const [pageCount, setPageCount] = useState(0);
+
   useEffect(() => {
-    TweenLite.staggerFrom(blogList.children, 1.5, {
-      delay: .3,
-      opacity: 0,
-      ease: Power3.easeIn
-    }, .3)
-      .staggerFrom(pagination.children, .5, {
-        opacity: 0,
-        y: -30,
-        ease: Power3.easeIn
-      }, .25, "-=1")
+    setPageCount(Math.floor(blogList.length/8));
   }, []);
 
+  useEffect(() => {
+    var tl = new TimelineLite();
+    tl.to(blogListHtml, 1.5, {
+      opacity: 1,
+      ease: Power3.linear
+    }).staggerFrom(pagination.children, .5, {
+      opacity: 0,
+      y: -30,
+      ease: Power3.easeIn
+    }, .25)
+  }, []);
+  
+  const setPagination = () => {
+    let paginationArray = [];
+
+    for(let i=0;pageCount+1;i++){
+    paginationArray.push(<div className="page">{i+1}</div>)
+    }
+
+    return paginationArray;
+
+  }
+
   return (
-      <div className="main-page">
-        <div ref={el => { blogList = el }} className="blogs">
-          <div className="blog-box">
-            <img src={computer} />
+    <div className="main-page">
+      {blogList == null ? <h2>Loading</h2> : null}
+      <div ref={el => { blogListHtml = el }} className="blogs">
+
+        {blogList.map((blog, index) => (
+          <div key={index} className="blog-box">
+            <img src={blog.mainPicture} />
             <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <Link href="/[blogId]" as="/Deneme"><a className="btn">Devamını Oku</a></Link>
+              <p>{blog.title}</p>
+              <Link href="/[blogId]" as={`/${blog.slug}`}><a className="btn">Devamını Oku</a></Link>
             </div>
           </div>
-          <div className="blog-box">
-            <img src={code} />
-            <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <button className="btn">Devamını Oku</button>
-            </div>
-          </div>
-          <div className="blog-box">
-            <img src={code2} />
-            <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <button className="btn">Devamını Oku</button>
-            </div>
-          </div>
-          <div className="blog-box">
-            <img src={computer} />
-            <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <button className="btn">Devamını Oku</button>
-            </div>
-          </div>
-          <div className="blog-box">
-            <img src={code2} />
-            <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <button className="btn">Devamını Oku</button>
-            </div>
-          </div>
-          <div className="blog-box">
-            <img src={code2} />
-            <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <button className="btn">Devamını Oku</button>
-            </div>
-          </div>
-          <div className="blog-box">
-            <img src={code2} />
-            <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <button className="btn">Devamını Oku</button>
-            </div>
-          </div>
-          <div className="blog-box">
-            <img src={code2} />
-            <div className="blog-summary">
-              <p>BLOG TITLE SAMPLE</p>
-              <button className="btn">Devamını Oku</button>
-            </div>
-          </div>
-        </div>
-        <div ref={el => { pagination = el }} className="pagination">
-          <div className="page"><img src={next} /></div>
-          <div className="page">1</div>
-          <div className="page">2</div>
-          <div className="page">3</div>
-          <div className="page"><img src={next} /></div>
-        </div>
-        <style jsx>{styles}</style>
+        ))}
+
       </div>
-      
+      <div ref={el => { pagination = el }} className="pagination">
+        <div className="page"><img src={next} /></div>
+        {
+          setPagination()
+        }
+        <div className="page"><img src={next} /></div>
+      </div>
+      <style jsx>{styles}</style>
+    </div>
+
 
 
   );
+};
+
+Home.getInitialProps = async ({ req, query }) => {
+  const querysnapshot = await firestore.collection("bloglar").get();
+  var list = [];
+  querysnapshot.forEach(doc => {
+    list.push(doc.data());
+  });
+
+  return {
+    blogList: list
+  }
 };
 
 export default Home;
