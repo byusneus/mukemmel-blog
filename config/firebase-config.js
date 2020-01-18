8c3efa9855bb33a56eb0bf082cd04927371b35c3
Yunus Emre Alpak <byusneus@gmail.com>
2020-01-18 16:14:44 +0300

    Heroku Settings

---
+++ config/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8gT0ilkEKecK7EoihRJNlBq9lEFv5xyU",
+    authDomain: "buse-blog.firebaseapp.com",
+    databaseURL: "https://buse-blog.firebaseio.com",
+    projectId: "buse-blog",
+    storageBucket: "buse-blog.appspot.com",
+    messagingSenderId: "149564831487",
+    appId: "1:149564831487:web:691a34dd7e613cf0237eca",
+    measurementId: "G-HJV7V9CT96"
+};
+
+export default firebaseConfig;
